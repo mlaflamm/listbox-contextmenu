6e405f6365fb40e9af8b0852a3d58eb7b82619bb
Manuel Laflamme <manuel.laflamme@gmail.com>
2011-12-03 23:44:53 -0500

    More woes when the listbox is a user control

---
+++ WpfListContextMenu/ListBoxUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfListContextMenu
+{
+	/// <summary>
+	/// Interaction logic for ListBoxUserControl.xaml
+	/// </summary>
+	public partial class ListBoxUserControl : UserControl
+	{
+		public ListBoxUserControl()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
